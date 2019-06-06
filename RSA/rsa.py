b2bb48a2669876967fc2de5a98d4c3b3d7ebc89c
conejo11 <gabrielgconejo@gmail.com>
2019-06-06 05:41:37 -0300

    fermat and extended euclidean

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -8,7 +8,7 @@ def extendedEuclidean(a, b):
 		gcd, x, y = extendedEuclidean(b % a, a)
 		return (gcd, y - (b/a) * x, x)
 
-def fermat(n):
+def fermatPrimalityTest(n):
     if n > 1:
         for t in range(5):
             rand = random.randint(2, n)-1
