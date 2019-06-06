a31abdc16faafe08a7a6c639f30ba99a754f4f49
conejo11 <gabrielgconejo@gmail.com>
2019-06-06 04:54:18 -0300

    fermat and extended euclidean

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -1,10 +1,22 @@
-def primalityTest():
-    
+import math
+import random
 
+def extendedEuclidean(a, b):
+	if a == 0:
+		return (b, 0, 1)
+	else:
+		gcd, x, y = extendedEuclidean(b % a, a)
+		return (gcd, y - (b/a) * x, x)
 
-
-
-
+def fermat(n):
+    if n > 1:
+        for t in range(5):
+            rand = random.randint(2, n)-1
+            if pow(rand,n-1,n) != 1:
+                return False
+        return True
+    else:
+        return False
 
 # // C++ program to find the smallest twin in given range
 # #include <bits/stdc++.h>
