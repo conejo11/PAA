1175ccf57c47c341bf95888782fb356f75e44007
conejo11 <gabrielgconejo@gmail.com>
2019-06-06 10:34:17 -0300

    lalalala agora vai

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -6,14 +6,63 @@ def extendedEuclidean(a, b):
 		return (b, 0, 1)
 	else:
 		gcd, x, y = extendedEuclidean(b % a, a)
-		return (gcd, y - (b/a) * x, x)
+		return (int(gcd), int(y - (b/a) * x), int(x))
 
 def fermatPrimalityTest(n):
     if n > 1:
-        for t in range(5):
+        for t in range(50):
             rand = random.randint(2, n)-1
             if pow(rand,n-1,n) != 1:
                 return False
         return True
     else:
         return False
+
+def geraPrimo(bits):
+    n = random.randint(0, pow(2, bits))
+    if n % 2 == 0:
+        n = n+1
+
+    while not fermatPrimalityTest(n):
+        print(n)
+        n += 2
+
+    return n
+
+def chavePublica(bits):
+    p = geraPrimo(bits)
+    q = geraPrimo(bits)
+
+    e = geraPrimo(8)
+    while math.gcd(e, (p-1)*(q-1)) != 1:
+        e = geraPrimo(16)
+
+    return (e, p, q)
+
+def chavePrivada(e, p, q):
+    d = extendedEuclidean(e, (p-1)*(q-1))[1]
+    #if d < 0:
+    #    d = d % ((p-1)*(q-1))
+
+    return (d, p * q)
+
+def criptografa(mensagem, e, n):
+    cript = []
+    for c in mensagem:
+        cript.append(pow(ord(c), e, n))
+
+    return cript
+
+def descriptografa(mensagem, d, n):
+    decript = []
+    for i in mensagem:
+        decript.append(chr(int(pow(i, d, n)%256)))
+
+    return decript
+
+#print(chavePrivada(17, 11, 13))
+e, p, q = chavePublica(32)
+d, n = chavePrivada(e, p, q)
+
+a = criptografa("abacate", e, n)
+print(descriptografa(a, d, n))
